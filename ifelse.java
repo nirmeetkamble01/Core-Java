196516d0b27bea856a40332461c9d6c8ded083e4
nirmeetkamble01 <107303479+nirmeetkamble01@users.noreply.github.com>
2022-08-18 20:37:20 +0530

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,15 @@
+class ifelse {
+    public static void main(String [] args) {
+        int a , b;
+        a = 10;
+        b =7;
+        if(a>b){
+            System.out.println("A is bigger than b");
+        }
+        else{
+            System.out.println("B is bigger than A");
+        }
+        
+    }
+    
+}
