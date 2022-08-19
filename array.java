d33780f7d1978715d4d81fa2b9b97c8fc45cfe08
nirmeetkamble01 <107303479+nirmeetkamble01@users.noreply.github.com>
2022-08-20 02:59:42 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Arrya
+{
+public static void main(String args [])
+{
+    int i;
+int []num = new int[6];
+Scanner s1 = new Scanner(System.in);
+for(i=0;i<6;i++)
+
+{
+    num[i] =s1.nextInt();
+}
+1
+}
+
+
+}
